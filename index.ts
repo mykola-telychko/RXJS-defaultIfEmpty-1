3399bb523dda2ca3a6da4c6721c44b4480cea0c0
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-12 12:04:16 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import { defaultIfEmpty } from 'rxjs/operators';
 import { of } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/conditional/defaultifempty
+// Example 1: Default for empty value
+
 // example 1
 //emit 'Observable.of() Empty!' when empty, else any values from source
 const exampleOne = of().pipe(defaultIfEmpty('Observable.of() Empty!'));
